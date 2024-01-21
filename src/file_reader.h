2a7f2c9f6d443b8bd6e9f3443b7e933b4fb81b24
PavelEnodd <pavelenodd@gmail.com>
2024-01-21 18:55:42 +1000

    создал основу проекта

--- src/file_reader.h
+++ src/file_reader.h
@@ -4,16 +4,35 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
+using namespace std;
+using json = nlohmann::json;
+
 class FileReader {
  private:
-  std::string adress_;
-  std::fstream file_;
+  json json_file_;
+
+  void LoadJson(const string& address) {
+    ifstream file(address);
+    if (file.is_open()) {
+      file >> json_file_;
+      cerr << "Settings file open!" << endl;
+    } else {
+      cerr << "Settings file not open!" << endl;
+      throw runtime_error("Unable to open JSON file.");
+    }
+  }
 
  public:
-  FileReader(/* args */);
-  ~FileReader();
+  FileReader() = default;  // Конструктор по умолчанию
+  FileReader(const string& address) { LoadJson(address); }
+
+  template <typename T>
+  T Get_Info(const string& variable_name) const {
+    try {
+      return json_file_.at(variable_name).get<T>();
+    } catch (const json::exception& e) {
+      cerr << "Error parsing JSON: " << e.what() << endl;
+      throw;
+    }
+  }
 };
-
-FileReader::FileReader(/* args */) {}
-
-FileReader::~FileReader() {}
